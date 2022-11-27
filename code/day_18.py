b6e8ebff8f962c5c97b79c386c95e4d226d608c6
Satrya Sabeni <satrya@Satryas-MacBook-Air.local>
2022-11-27 11:52:00 +0100

    somehow working shit

--- code/day_18.py
+++ code/day_18.py
@@ -5,25 +5,31 @@ def dissect(nodes, level = []):
     right_node = nodes[1]
     depth = len(level)
 
-    if type(left_node) is int:
-        print('end L', left_node, level + [0])
-        return level
-    else:
-        if (depth + 1) == 4:
-            print('explosion!')
-            return level
-        print(left_node, level + [0])
-        dissect(left_node, level + [0])
+    if type(nodes) is int:
+        print('end_node')
+        return
 
-    if type(right_node) is int:
-        print('end R', right_node, level + [1])
+    # if type(left_node) is int:
+    #     print('end L', left_node, level + [0])
+    #     return level
+    # else:
+    left_level = level + [0]
+    if len(left_level) == 4:
+        print('explosion!')
         return level
-    else:
-        if (depth + 1) == 4:
-            print('explosion')
-            return level
-        dissect(right_node, level + [1])
-        #print('dissect', right_node, level + [1])
+    print(left_node, level + [0])
+    dissect(left_node, level + [0])
+
+    # if type(right_node) is int:
+    #     print('end R', right_node, level + [1])
+    #     return level
+    # else:
+    right_level = level + [0]
+    if (depth + 1) == 4:
+        print('explosion')
+        return level
+    dissect(right_node, level + [1])
+    #print('dissect', right_node, level + [1])
 
 
 test_nodes = [[[[[4,3],4],4],[7,[[8,4],9]]], [1, 1]]
